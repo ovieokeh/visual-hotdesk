69af84d130ad2a6f8b1f52f13c152c5bc11e3556
Ovie Okeh <kevinokeh@gmail.com>
2019-08-29 12:50:33 +0100

    fix puppeteer issue on Heroku

--- src/helpers/generateImageBuffer.js
+++ src/helpers/generateImageBuffer.js
@@ -2,7 +2,10 @@ const puppeteer = require('puppeteer');
 
 async function generateImageBuffer(html) {
   const start = Date.now();
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.setContent(html);
   const ssBuffer = await page.screenshot({
